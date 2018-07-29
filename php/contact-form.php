38302177619908a750809a979494372e569e3601
Mario Oliveira <mario.oliveirag12@gmail.com>
2018-07-29 15:02:54 -0300

    Upload Projeto

---
+++ php/contact-form.php
@@ -0,0 +1,22 @@
+﻿{"response":"success"}
+
+
+<?php
+
+    $to = "contao@adlespumas.com.br";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "Uma email do Weboliveira.";
+
+    $fields = array();
+    $fields{"name"} = "Nome";
+    $fields{"email"} = "Email";
+    $fields{"subject"} = "Assunto";
+    $fields{"message"} = "Messagem";
+
+    $body = "Aqui está o email ADL Espuma que foi enviado:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
